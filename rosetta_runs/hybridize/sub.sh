167b861ec951bea9a2e8837cd112b9d5f290fc95
Alex Carlin <carlin@ucdavis.edu>
2016-05-26 21:32:42 -0700

    Adding all files from homology modeling run

--- rosetta_runs/hybridize/sub.sh
+++ rosetta_runs/hybridize/sub.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH --output=logs/slurm-%A_%a.out
 #SBATCH --error=logs/slurm-%A_%a.err
-#SBATCH --job-name=bglbench
+#SBATCH --job-name=hybridize
 #SBATCH -n 1
-#SBATCH -t 0-1:00
+#SBATCH -t 0-24:00
 
 #=$( sed -n "$SLURM_ARRAY_TASK_ID p" list )
 /share/work/rosetta/source/bin/rosetta_scripts.linuxgccrelease @flags
